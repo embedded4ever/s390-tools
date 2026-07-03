embedded4ever/s390-tools

commit 60bc1e38d058c2f90c89780f0e086b66ec4a76a0

zipl/src: Reuse data of buffer components in bootmap
Don't duplicate data of components added via add_component_buffer()
and friends to bootmap file. Instead, reuse data that were previously
added when preparing a program table for the first mirror to create
metadata (block lists, program tables, etc) specific for other mirrors.


Signed-off-by: Eduard Shishkin <edward6@linux.ibm.com>
Reviewed-by: Stefan Haberland <sth@linux.ibm.com>
Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>

zipl/src/bootmap.c
@@ -480,6 +480,14 @@ static int add_component_buffer_align(struct install_set *bis, void *buffer,
 		error_text("Could not write to bootmap file");
 		return -1;
 	}
+	if (!bis->comp_reg[comp_id].offset) {
+		/*
+		 * save component offset and size
+		 */
+		assert(offset && *offset);
+		bis->comp_reg[comp_id].offset = *offset;
+		bis->comp_reg[comp_id].len = size;
+	}
 	if (component_type_by_id(comp_id) == COMPONENT_TYPE_LOAD) {
 		/* Fill in component location */
 		location->addr = data.load_address;
@@ -510,9 +518,26 @@ static int add_component_buffer(struct install_set *bis, void *buffer,
 				int mirror_id, int program_table_id)
 {
 	struct disk_info *info = &bis->info->base[mirror_id];
+	loff_t offset;
 
+	if (bis->comp_reg[comp_id].offset > 0) {
+		/*
+		 * The component data has been already written to the
+		 * bootmap. Refer the respective region in the bootmap
+		 * file.
+		 */
+		return add_component_file_range(bis,
+						bis->filename /* bootmap */,
+						&bis->comp_reg[comp_id],
+						data.load_address,
+						0 /*trailer */,
+						component,
+						0 /* do not add file data*/,
+						comp_id, menu_idx, mirror_id,
+						program_table_id);
+	}
 	return add_component_buffer_align(bis, buffer, size, data, component,
-					  info->phy_block_size, NULL,
+					  info->phy_block_size, &offset,
 					  comp_id, menu_idx, mirror_id,
 					  program_table_id);
 }

commit 4f622325efa2e433254ea01d3eabf472dcc6f757

zipl/src: Fix add_component_file_range()
Fix add_component_file_range() to process components of all types
(not only of COMPONENT_TYPE_LOAD), similar to how it is going in
add_component_buffer().

Earlier components of COMPONENT_TYPE_SIGNATURE were processed only
by add_component_buffer(). Starting from 60bc1e3, signatures for
mirrors (i.e. base disks of #1 and larger ID) are processed also by
the function add_component_file_range(), which works incorrectly for
such type of components.

Fixes: 60bc1e38d058 ("zipl/src: Reuse data of buffer components in bootmap")
Reviewed-by: Stefan Haberland <sth@linux.ibm.com>
Signed-off-by: Eduard Shishkin <edward6@linux.ibm.com>
Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

zipl/src/bootmap.c
@@ -413,8 +413,13 @@ static int add_component_file_range(struct install_set *bis,
 		*count -= DIV_ROUND_UP(trailer, info->phy_block_size);
 	}
 	/* Fill in component location */
-	location->addr = load_address;
-	location->size = *count * info->phy_block_size;
+	if (component_type_by_id(comp_id) == COMPONENT_TYPE_LOAD) {
+		location->addr = load_address;
+		location->size = *count * info->phy_block_size;
+	} else {
+		location->addr = 0;
+		location->size = 0;
+	}
 	/* Try to compact list */
 	*count = disk_compact_blocklist(*list, *count, info);
 write_segment_table: