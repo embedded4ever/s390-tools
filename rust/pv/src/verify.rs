embedded4ever/s390-tools

commit c6f621d0dc8b7a8934d728df34239b959cabc718

rust: Add library for pv tools
Add a `pv` crate that bundles useful functions and structs for creating
requests like `Attestation`, `Add Secret`, or even `Boot` a.k.a.
Secure Execution Image.
Note pv includes a subcrate `openssl_extensions` that (temporarily)
bundles some needed `openssl-rust` functionalities that are not
upstream yet. The plan is to remove these, when they become
upstream.

The pv crate has multiple features:
 * request - code to generate requests
 * uvsecret - code to access the UV-secret api
		with request enabled also generating requests is
		possible

Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>
Acked-by: Jan Höppner <hoeppner@linux.ibm.com>
Acked-by: Marc Hartmayer <mhartmay@linux.ibm.com>
Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

rust/pv/src/verify.rs
@@ -1,0 +1,158 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright IBM Corp. 2023
+
+use core::slice;
+use log::debug;
+use openssl::stack::Stack;
+use openssl::x509::store::X509Store;
+use openssl::x509::{CrlStatus, X509Ref, X509StoreContext, X509};
+use openssl_extensions::crl::StackableX509Crl;
+use openssl_extensions::crl::X509StoreContextExtension;
+
+use crate::error::bail_hkd_verify;
+use crate::misc::{read_certs, read_file};
+use crate::Result;
+
+mod helper;
+mod test;
+
+/// A HkdVerifier verifies that a host-key document(HKD) can be trusted.
+///
+/// If the verification fails the HKD should not be used to create requests.
+pub trait HkdVerifier {
+    /// Checks if the given host-key document can be trusted.
+    ///
+    /// #Errors
+    ///
+    /// This function will return an error if the Hostkey cannot be trusted.
+    /// Refer to the concrete Error type for the specific reason.
+    fn verify(&self, hkd: &X509Ref) -> Result<()>;
+}
+
+/// A "verifier" that does not verify and accepts all given host-keys as valid.
+pub struct NoVerifyHkd;
+impl HkdVerifier for NoVerifyHkd {
+    fn verify(&self, _hkd: &X509Ref) -> Result<()> {
+        Ok(())
+    }
+}
+
+/// A Verifier that checks the host-key document against a chain of trust.
+pub struct CertVerifier {
+    store: X509Store,
+    ibm_z_sign_key: X509,
+    offline: bool,
+}
+
+impl HkdVerifier for CertVerifier {
+    /// This function verifies a host-key
+    /// document. To do so multiple steps are required:
+    ///
+    /// 1. issuer(host_key) == subject(ibm_z_sign_key)
+    /// 2. Signature verification
+    /// 3. @hkd must not be expired
+    /// 4. @hkd must not be revoked
+    fn verify(&self, hkd: &X509Ref) -> Result<()> {
+        helper::verify_hkd_options(hkd, &self.ibm_z_sign_key)?;
+
+        // verify that the hkd was signed with the key of the IBM signing key
+        if !hkd.verify(self.ibm_z_sign_key.public_key()?.as_ref())? {
+            bail_hkd_verify!(Signature);
+        }
+
+        // Find matching crl for sign key in the store or download them
+        let crls = self.hkd_crls(hkd)?;
+
+        // Verify that the CLRs are still valid
+        let mut verified_crls = Vec::with_capacity(crls.len());
+        for crl in &crls {
+            if helper::verify_crl(crl, &self.ibm_z_sign_key).is_some() {
+                verified_crls.push(crl.to_owned());
+            }
+        }
+
+        // Test if hkd was revoked (min1 required)
+        if verified_crls.is_empty() {
+            bail_hkd_verify!(NoCrl);
+        }
+        for crl in &verified_crls {
+            match crl.get_by_cert(&hkd.to_owned()) {
+                CrlStatus::NotRevoked => (),
+                _ => bail_hkd_verify!(HdkRevoked),
+            }
+        }
+        debug!("HKD: verified");
+        Ok(())
+    }
+}
+
+impl CertVerifier {
+    ///Download the CLRs that a HKD refers to.
+    pub fn hkd_crls(&self, hkd: &X509Ref) -> Result<Stack<StackableX509Crl>> {
+        let mut ctx = X509StoreContext::new()?;
+        // Unfortunately we cannot use a dedicated function here and have to use a closure (E0434)
+        // Otherwise, we cannot refer to self
+        let mut crls = ctx.init_opt(&self.store, None, None, |ctx| {
+            let subject = self.ibm_z_sign_key.subject_name();
+            match ctx.crls(subject) {
+                Ok(crls) => Ok(crls),
+                _ => {
+                    // reorder the name and try again
+                    let broken_subj = helper::reorder_x509_names(subject)?;
+                    ctx.crls(&broken_subj).or_else(helper::stack_err_hlp)
+                }
+            }
+        })?;
+
+        if !self.offline {
+            // Try to download a CRL if defined in the HKD
+            if let Some(crl) = helper::download_first_crl_from_x509(hkd)? {
+                crl.into_iter().try_for_each(|c| crls.push(c.into()))?;
+            }
+        }
+        Ok(crls)
+    }
+}
+
+impl CertVerifier {
+    /// Create a `CertVerifier`.
+    ///
+    /// * `cert_paths` - Paths to Cerificates for the chain of trust
+    /// * `crl_paths` - Paths to certificate revocation lists for the chain of trust
+    /// * `root_ca_path` - Path to the root of trust
+    /// * `offline` - if set to true the verification process will not try to download CRLs from the
+    /// internet.
+    /// # Errors
+    ///
+    /// This function will return an error if the chain of trust could not be established.
+    pub fn new(
+        cert_paths: &[String],
+        crl_paths: &[String],
+        root_ca_path: &Option<String>,
+        offline: bool,
+    ) -> Result<Self> {
+        let mut store = helper::store_setup(root_ca_path, crl_paths, cert_paths)?;
+        let mut untr_certs = Vec::with_capacity(cert_paths.len());
+        for path in cert_paths {
+            let mut crt = read_certs(&read_file(path, "certificate")?)?;
+            if !offline {
+                helper::download_crls_into_store(&mut store, &crt)?;
+            }
+            untr_certs.append(&mut crt);
+        }
+
+        // remove the IBM signing certificate from chain.
+        // We have to verify them separately as they are not marked as intermediate certs
+        let (ibm_z_sign_key, chain) = helper::extract_ibm_sign_key(untr_certs)?;
+
+        let store = store.build();
+        helper::verify_chain(&store, &chain, slice::from_ref(&ibm_z_sign_key))?;
+
+        Ok(Self {
+            store,
+            ibm_z_sign_key,
+            offline,
+        })
+    }
+}

commit 187437c6c827e97acf820b1acc3faa67a8224e1d

pv: Defer CRL downloads until certificate validation succeeds
Perform certificate verification in two stages. First, verify the
certificate chain without CRL checks. Once the chain has been validated,
download the referenced CRLs and repeat the verification with CRL
checking enabled.

Fixes: c6f621d0dc8b ("rust: Add library for pv tools")
Signed-off-by: Marc Hartmayer <marc@linux.ibm.com>
Reviewed-by: Steffen Eiden <seiden@linux.ibm.com>
Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

rust/pv/src/verify.rs
@@ -5,7 +5,7 @@
 use core::slice;
 use std::path::Path;
 
-use helper::download_first_crl_from_x509;
+use helper::{download_first_crl_from_x509, StoreSetupMode};
 use log::{debug, trace};
 use openssl::error::ErrorStack;
 use openssl::stack::Stack;
@@ -181,25 +181,45 @@ impl CertVerifier {
         Q: AsRef<Path>,
         R: AsRef<Path>,
     {
-        let mut store = helper::store_setup(root_ca_path, crl_paths, cert_paths)?;
         let mut untr_certs = Vec::with_capacity(cert_paths.len());
         for path in cert_paths {
             let mut crt = read_certs(&read_file(path, "certificate")?)?;
-            if !offline {
-                for c in &crt {
-                    if let Some(crl) = download_first_crl_from_x509(c)? {
-                        crl.iter().try_for_each(|c| store.add_crl(c))?;
-                    }
-                }
-            }
             untr_certs.append(&mut crt);
         }
+        let (ibm_z_sign_key, chain) = helper::extract_ibm_sign_key(untr_certs.clone())?;
 
-        // remove the IBM signing certificate from chain.
-        // We have to verify them separately as they are not marked as intermediate certs
-        let (ibm_z_sign_key, chain) = helper::extract_ibm_sign_key(untr_certs)?;
+        // Two-round verification:
+        //
+        // Round 1: Verify chain without CRL checks before downloading files
+        // from URLs from (yet) untrusted certificates.
+        let store_builder = helper::store_setup(
+            root_ca_path.as_ref(),
+            crl_paths,
+            cert_paths,
+            StoreSetupMode::WithoutCrlCheck,
+        )?;
+        helper::verify_chain(
+            &store_builder.build(),
+            &chain,
+            slice::from_ref(&ibm_z_sign_key),
+        )?;
 
-        let store = store.build();
+        // Round 2: Download CRLs and verify again, but this time with CRL checks
+        let mut store_builder = helper::store_setup(
+            root_ca_path,
+            crl_paths,
+            cert_paths,
+            StoreSetupMode::WithCrlCheck,
+        )?;
+        if !offline {
+            for cert in &untr_certs {
+                if let Some(crls) = download_first_crl_from_x509(cert)? {
+                    crls.iter().try_for_each(|c| store_builder.add_crl(c))?;
+                }
+            }
+        }
+
+        let store = store_builder.build();
         helper::verify_chain(&store, &chain, slice::from_ref(&ibm_z_sign_key))?;
 
         Ok(Self {

rust/pv/src/verify/helper.rs
@@ -110,21 +110,27 @@ pub fn verify_crl(crl: &X509CrlRef, issuer: &X509Ref) -> Option<()> {
     }
 }
 
+pub enum StoreSetupMode {
+    WithCrlCheck,
+    WithoutCrlCheck,
+}
+
 /// Setup the x509Store such that it can be used it for verifying certificates
 pub fn store_setup<P: AsRef<Path>, Q: AsRef<Path>, R: AsRef<Path>>(
     root_ca_path: Option<P>,
     crl_paths: &[Q],
     cert_w_crl_paths: &[R],
+    mode: StoreSetupMode,
 ) -> Result<X509StoreBuilder> {
-    let mut x509store = X509StoreBuilder::new()?;
+    let mut x509store_builder = X509StoreBuilder::new()?;
 
     match root_ca_path {
-        None => x509store.set_default_paths()?,
-        Some(p) => load_root_ca(p, &mut x509store)?,
+        None => x509store_builder.set_default_paths()?,
+        Some(p) => load_root_ca(p, &mut x509store_builder)?,
     }
 
     for crl in crl_paths {
-        load_crl_to_store(&mut x509store, crl, true).map_err(|source| Error::X509Load {
+        load_crl_to_store(&mut x509store_builder, crl, true).map_err(|source| Error::X509Load {
             path: crl.as_ref().into(),
             ty: Error::CRL,
             source,
@@ -132,27 +138,35 @@ pub fn store_setup<P: AsRef<Path>, Q: AsRef<Path>, R: AsRef<Path>>(
     }
 
     for crl in cert_w_crl_paths {
-        load_crl_to_store(&mut x509store, crl, false).map_err(|source| Error::X509Load {
-            path: crl.as_ref().into(),
-            ty: Error::CRL,
-            source,
+        load_crl_to_store(&mut x509store_builder, crl, false).map_err(|source| {
+            Error::X509Load {
+                path: crl.as_ref().into(),
+                ty: Error::CRL,
+                source,
+            }
         })?;
     }
     let mut param = X509VerifyParam::new()?;
-    let flags = X509VerifyFlags::X509_STRICT
-        | X509VerifyFlags::CRL_CHECK
-        | X509VerifyFlags::CRL_CHECK_ALL
+    let mut flags = X509VerifyFlags::X509_STRICT
         | X509VerifyFlags::TRUSTED_FIRST
         | X509VerifyFlags::CHECK_SS_SIGNATURE
         | X509VerifyFlags::POLICY_CHECK;
+    match mode {
+        StoreSetupMode::WithCrlCheck => {
+            flags |= X509VerifyFlags::CRL_CHECK | X509VerifyFlags::CRL_CHECK_ALL
+        }
+        StoreSetupMode::WithoutCrlCheck => {
+            // nothing to do
+        }
+    }
 
     param.set_depth(SECURITY_CHAIN_MAX_LEN);
     param.set_auth_level(SECURITY_LEVEL as i32);
     param.set_purpose(X509PurposeId::ANY)?;
     param.set_flags(flags)?;
-    x509store.set_param(&param)?;
+    x509store_builder.set_param(&param)?;
 
-    Ok(x509store)
+    Ok(x509store_builder)
 }
 
 /// Verify that the given IBM signing keys can be trusted

rust/pv/src/verify/test.rs
@@ -9,6 +9,7 @@ use openssl::stack::Stack;
 use super::helper::*;
 use super::{helper, *};
 use crate::test_utils::*;
+use crate::verify::helper::StoreSetupMode;
 use crate::Error;
 use crate::HkdVerifyErrorType::*;
 
@@ -18,7 +19,12 @@ fn store_setup() {
     let inter_path = get_cert_asset_path("inter.crt");
     let crls: [String; 0] = [];
 
-    let store = helper::store_setup(None::<String>, &crls, &[&ibm_path, &inter_path]);
+    let store = helper::store_setup(
+        None::<String>,
+        &crls,
+        &[&ibm_path, &inter_path],
+        StoreSetupMode::WithCrlCheck,
+    );
     assert!(store.is_ok());
 }
 
@@ -41,9 +47,14 @@ fn verify_chain_offline() {
     let root_crt = get_cert_asset_path("root_ca.chained.crt");
     let certs: [String; 0] = [];
 
-    let store = helper::store_setup(Some(&root_crt), &[&inter_crl], &certs)
-        .unwrap()
-        .build();
+    let store = helper::store_setup(
+        Some(&root_crt),
+        &[&inter_crl],
+        &certs,
+        StoreSetupMode::WithCrlCheck,
+    )
+    .unwrap()
+    .build();
 
     let mut sk = Stack::<X509>::new().unwrap();
     sk.push(inter_crt).unwrap();